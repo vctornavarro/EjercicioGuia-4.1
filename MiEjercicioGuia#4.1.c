ce4faaccc0b8494dc92fc5324db91eda9534f5e1
vctornavarro <victor.navarro1@estudiantat.upc.edu>
2023-10-10 12:38:37 +0200

    4.1

--- MiEjercicioGuia#4.1.c
+++ MiEjercicioGuia#4.1.c
@@ -1,32 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define MAX 25
 
-int main(int argc, char *argv[]) {
-	
-	char peticion[100];
-	char respuesta[100];
-	
-	strcpy (peticion, "Jofre/33/Oriol/25/Victor/19/Jose/79");
-	//resultado: Jofre*33-Oriol*25-Victor*19-Jose*79
-	
-	char nombre[20];
-	int edad;
-	char *p = strtok (peticion, "/");
-	
-	while (p!=NULL)
-	{
-		strcpy (nombre, p);
-		p = strtok (NULL, "/");
-		edad = atoi (p);
-		if (edad > 18)
-			sprintf (respuesta, "%s%s*%d-", respuesta, nombre, edad);
-		
-		p = strtok (NULL, "/");
-	}
-	respuesta [strlen (respuesta) -1] = '\0';
-	printf ("Resultado: %s\n", respuesta);
+int main(int argc, char argv[]) {
+
+    char peticion[100];
+    char respuesta[100];
+
+    strcpy (peticion, "Jofre/33/Oriol/25/Victor/19/Jose/79");
+    //resultado: Jofre*33-Oriol*25-Victor*19-Jose*79
+
+    char nombre[10];
+    int edad;
+
+    charp = strtok (peticion, "/");
+    strcpy (nombre,p);
+    p = strtok (NULL, "/");
+    edad = atoi (p);
+    if (edad>=18)
+        sprintf (respuesta,"%s%s%d-", respuesta,nombre,edad);
+    p = strtok (NULL, "/");
+    strcpy (nombre,p);
+    p= strtok (NULL,"/");
+    edad = atoi (p);
+    if (edad>=18)
+        sprintf (respuesta,"%s%s%d-", respuesta,nombre,edad);
+    p = strtok (NULL, "/");
+    strcpy (nombre,p);
+    p= strtok (NULL,"/");
+    edad = atoi (p);
+    if (edad>=18)
+        sprintf (respuesta,"%s%s%d-", respuesta,nombre,edad);
+    p = strtok (NULL, "/");
+    strcpy (nombre,p);
+    p= strtok (NULL,"/");
+    edad = atoi (p);
+    if (edad>=18)
+        sprintf (respuesta,"%s%s%d", respuesta,nombre,edad);
+    printf ("Resultado: %s\n", respuesta);
+    return 0;
+
 	
 	int p_i, p_f;
 	char Palabra[MAX], caracter;
